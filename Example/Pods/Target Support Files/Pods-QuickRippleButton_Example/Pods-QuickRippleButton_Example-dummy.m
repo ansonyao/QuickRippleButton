75f8e11598abfa21c5227925e422dbaa28d7ccff
Anson Yao <yaoenxin@gmail.com>
2016-11-07 23:04:22 -0800

    Remove pods

--- Example/Pods/Target Support Files/Pods-QuickRippleButton_Example/Pods-QuickRippleButton_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QuickRippleButton_Example : NSObject
-@end
-@implementation PodsDummy_Pods_QuickRippleButton_Example
-@end
--- Example/Pods/Target Support Files/QuickRippleButton/QuickRippleButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QuickRippleButton : NSObject
-@end
-@implementation PodsDummy_QuickRippleButton
-@end
